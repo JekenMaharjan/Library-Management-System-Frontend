488b7dff6534642e25d0a12def525d3758197920
JekenMaharjan <maharjanjeken@gmail.com>
2026-01-22 17:48:10 +0545

    GET and DELETE method implement on book page

--- client/app/book/page.tsx
+++ client/app/book/page.tsx
@@ -1,7 +1,38 @@
-import Sidebar from '@/components/Sidebar'
-import React from 'react'
+"use client"
 
-const BookPage = () => {
+import Sidebar from '@/components/Sidebar'
+import { deleteBook, deleteStudent, getBooks, getStudents } from '@/lib/api';
+import React, { useEffect, useState } from 'react'
+
+type Book = {
+    bookId: number;
+    title: string;
+    author: string;
+    totalStock: number;
+};
+
+const StudentPage = () => {
+    const [books, setBooks] = useState<Book[]>([]);
+
+    useEffect(() => {
+        loadBooks();
+    }, []);
+
+    const loadBooks = async () => {
+        try {
+            const data = await getBooks();
+            setBooks(data);
+        }
+        catch (error) {
+            console.error("Failed to fetch students", error);
+        }
+    };
+
+    const handleDelete = async (id: number) => {
+        await deleteBook(id);
+        loadBooks(); // refresh list
+    };
+
     return (
         <div className="flex">
             {/* Sidebar (fixed) */}
@@ -17,11 +48,43 @@ const BookPage = () => {
 
                 {/* Page content */}
                 <main className="flex-1 bg-gray-100 p-6 overflow-auto">
+                    <h1 className="text-2xl font-semibold mb-6">Students</h1>
 
+                    <div className="bg-white rounded-lg shadow overflow-hidden">
+                        {/* Header */}
+                        <div className="grid grid-cols-4 font-semibold bg-gray-300 px-6 py-3">
+                            <span>Title</span>
+                            <span>Author</span>
+                            <span>Total Stock</span>
+                            <span className="text-center">Actions</span>
+                        </div>
+
+                        {/* Row */}
+                        {books.map((book) => (
+                            <div
+                                key={book.bookId}
+                                className="grid grid-cols-4 font-mono items-center px-6 py-2 hover:bg-gray-50 border-t border-t-gray-300">
+                                <p>{book.title}</p>
+                                <p>{book.author}</p>
+                                <p>{book.totalStock}</p>
+
+                                <div className="flex font-semibold justify-center gap-3">
+                                    <button className="bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded-md text-sm">
+                                        Edit
+                                    </button>
+                                    <button
+                                        onClick={() => handleDelete(book.bookId)}
+                                        className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded-md text-sm">
+                                        Delete
+                                    </button>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
                 </main>
             </div>
         </div>
     )
 }
 
-export default BookPage
+export default StudentPage
--- client/lib/api.tsx
+++ client/lib/api.tsx
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const API_BASE_URL = "http://localhost:5000/api"; // change if needed
 
+// Students
 export const getStudents = async () => {
     const response = await axios.get(`${API_BASE_URL}/students`);
     return response.data;
@@ -13,4 +14,14 @@ export const deleteStudent = async (id: number) => {
 
 export const updateStudent = async (id: number, data: any) => {
     return axios.put(`${API_BASE_URL}/students/${id}`, data);
+};
+
+// Books
+export const getBooks = async () => {
+    const response = await axios.get(`${API_BASE_URL}/books`);
+    return response.data;
+};
+
+export const deleteBook = async (id: number) => {
+    return axios.delete(`${API_BASE_URL}/books/${id}`);
 };
